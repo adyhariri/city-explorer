47534daf766a107ef111582e4b1a3b63e88c255f
adyhariri <mahmoudalhariri1994@gmail.com>
2021-06-16 18:55:20 +0300

    lab07 done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Weather from './weather';
 
 class App extends React.Component{
   constructor(props){
@@ -8,7 +9,9 @@ class App extends React.Component{
       locationData:'',
       errorMessage:'',
       displayErrMes:false,
-      displayImg:false
+      displayImg:false,
+      cData:'',
+      displayWeather:false
     }
   }
   getLoc= async (event)=>{
@@ -33,11 +36,34 @@ class App extends React.Component{
     }
 
   }
+  getWeather=async (event)=>{
+    event.preventDefault();
+    let query=event.target.search.value;
+    let weatherUrl=`${process.env.REACT_APP_WEATHERURL}?city_name=${query}`;
+    console.log(weatherUrl);
+    try{
+      let res=await axios.get(weatherUrl);
+      this.setState({
+        cData:res.data,
+        displayWeather:true
+        
+      })
+      console.log(res.data)
+    }
+    catch{
+      this.setState({
+
+        errorMessage:'city not found',
+        displayWeather:false
+      })
+    }
+  }
   render(){
     return(
       <div>
         <h1>City Explorer</h1>
-        <form onSubmit={this.getLoc}>
+        <form onSubmit={(event)=>{this.getLoc(event);
+          this.getWeather(event);}}>
           <input type='text' placeholder='city name' name='search' />
           <input type='submit' value='Explore!' />
         </form>
@@ -45,7 +71,12 @@ class App extends React.Component{
         <p>latitude :{this.state.locationData.lat}</p>
         <p>longitude:{this.state.locationData.lon}</p>
         {this.state.displayImg&&<img src={`https://maps.locationiq.com/v3/staticmap?key=pk.005b36a82b2d0a1ca8e0cba89b50a48d&center=${this.state.locationData.lat},${this.state.locationData.lon}`}alt='map'/>}
-        { this.state.displayErrMes&&this.state.errorMessage}
+        {/* { this.state.displayErrMes&&this.state.errorMessage} */}
+        {this.state.displayWeather&&this.state.cData.map((item)=>(
+          
+           <Weather des={item.des} date={item.date}/>
+
+        ))}
       </div>
     )
   }
